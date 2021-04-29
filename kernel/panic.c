69a2edee1a766404edca1d12b9c96abb951b89ae
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-29 15:04:14 -0700

    panic file

---
+++ kernel/panic.c
@@ -0,0 +1,11 @@
+/********************************************************************
+ * Simple panic file                                                *
+ * (c) Copyleft 2021 DamieFC                                        *
+ ********************************************************************/
+#include <stdio.h>
+#include <stddef.h>
+
+void panic(void){
+    printf(">----- PANIC -----<\n");
+    printf("Something happened. No idea what. Oh well. Everything has been lost that was unsaved. Unplug and replug the computer.\n");
+}
