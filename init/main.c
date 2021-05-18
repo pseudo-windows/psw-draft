8ea0455ae3df145d979ed915a47c8e1a1862974b
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-18 10:19:26 -0700

    Update main.c

--- init/main.c
+++ init/main.c
@@ -3,3 +3,8 @@
  * GPLv3.0                                                    *
  * (c) DamieFC 2021                                           *
  **************************************************************/
+#include <stdio.h>
+
+void *kernel_main(){
+  printf("Hi there");
+}
